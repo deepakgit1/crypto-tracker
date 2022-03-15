b0fd0f0534dea03402490bd80d439f870dc5b028
deepakgit1 <dakshgaidhane1@gmal.com>
2022-03-15 20:10:48 +0530

    make it better

---
+++ src/components/Header.js
@@ -0,0 +1,61 @@
+import { AppBar, Container, createTheme, makeStyles, MenuItem, Select, ThemeProvider, Toolbar, Typography } from '@material-ui/core'
+import React from 'react'
+import {useNavigate} from 'react-router-dom'
+import { CryptoState } from '../CryptoContext'
+import AuthModel from './Authentication/AuthModel'
+import UserSidebar from './Authentication/UserSidebar'
+
+const useStyles = makeStyles(()=>({
+    title: {
+        flex: 1,
+        color: "gold",
+        fontFamily: "Montserrat",
+        fontWeight: "bold",
+        cursor: "pointer",
+    }
+}))
+const Header = () => {
+
+    const classes =useStyles()
+    const history = useNavigate()
+
+    const {currency,setCurrency,user} = CryptoState()
+    console.log(currency);
+
+    const darktheme = createTheme({
+        palette:{
+            primary:{
+                main:"#fff",
+            },
+     
+        type:"dark"
+        }
+    })
+
+  return (
+      <ThemeProvider theme={darktheme}>
+    <AppBar color='transparent' position='static'>
+        <Container>
+            <Toolbar>
+                <Typography onClick={()=> history("/")} className={classes.title} >Crypto Hunter</Typography>
+                <Select variant='outlined' style={{
+                    width:100,
+                    height: 40,
+                    marginRight:15
+                }}
+                value={currency}
+                onChange={(e)=>setCurrency(e.target.value)}
+                >
+                    <MenuItem value={"USD"}>USD</MenuItem>
+                    <MenuItem value={"INR"}>INR</MenuItem>
+                </Select>
+              { user?<UserSidebar/>:  <AuthModel/>}
+            </Toolbar>
+        </Container>
+
+    </AppBar>
+    </ThemeProvider>
+  )
+}
+
+export default Header
---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDEr1VHfSDEo0fwSmZleD_uN6_FpqQqkck",
+    authDomain: "crypto-tracker-3ecbb.firebaseapp.com",
+    projectId: "crypto-tracker-3ecbb",
+    storageBucket: "crypto-tracker-3ecbb.appspot.com",
+    messagingSenderId: "659676925290",
+    appId: "1:659676925290:web:77caec3f080cf8bdd1e913"
+  };
+
+  export default firebaseConfig;
